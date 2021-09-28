b5c99c7746ce839d6c9fac3605b5b04b5f45e19e
Rasme Akter <87582286+raakstmeer@users.noreply.github.com>
2021-09-28 11:19:56 +0600

    Add files via upload

---
+++ JavaScript/another_all_script.js
@@ -0,0 +1,8 @@
+// type js plagin control 
+
+var typed = new Typed('.attribute', {
+    strings: ['Tushar Imran','Web Designer','Graphics Designer','Video Editor'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
